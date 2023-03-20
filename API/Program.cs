e9635ad417346ed5d57a5ca5965defa79a42552a
ducdang93 <duc.dang@datagap.io>
2023-03-20 23:21:32 +0700

    add angular

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));    
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -22,7 +23,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
